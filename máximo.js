f0c3405831ea0f9335e56777aeadf01385b40ffc
alahozrodriguez <114485912+alahozrodriguez@users.noreply.github.com>
2023-03-21 13:11:21 +0100

    Create máximo.js

---
+++ máximo.js
@@ -0,0 +1,9 @@
+function maximo(a,b) {
+    if (a>b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+let resm = maximo(543,723);
+console.log('El mayor número entre el 543 y el 723 es el '+resm);
