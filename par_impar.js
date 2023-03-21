69a4aa60b16a74a4d556750f1bea1aeda67c630c
alahozrodriguez <114485912+alahozrodriguez@users.noreply.github.com>
2023-03-21 13:27:10 +0100

    Create par_impar.js

---
+++ par_impar.js
@@ -0,0 +1,10 @@
+funtion eṣ_par_impar(número){
+  if(número %2==0){
+    return "par";
+  }else {
+    
+    return "impar";
+  }
+}
+console. log ( "3-->"+ es par_impar (3));
+console.log ("máximo de 2 y 7 es" + solución);
